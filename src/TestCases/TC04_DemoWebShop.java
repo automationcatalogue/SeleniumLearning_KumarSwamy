f0a6e7ce273e844befadf9922fa85714e9842859
automationcatalogue <automation.catalogue@gmail.com>
2022-07-17 10:44:15 +0530

    TC04 testcases changes

--- src/TestCases/TC04_DemoWebShop.java
+++ src/TestCases/TC04_DemoWebShop.java
@@ -1,7 +1,12 @@
 package TestCases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TC04_DemoWebShop {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","");
+
+        System.setProperty("webdriver.chrome.driver","C:\\AutomationCatalogue\\Drivers\\Chrome\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
     }
 }
